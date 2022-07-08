9133e7a54599989e42551189bb0c227b28258366
Juanca <juanca-jimi@users.noreply.github.com>
2022-07-08 12:11:08 +0000

    changes to rpg_code

--- rpg_code.py
+++ rpg_code.py
@@ -2,6 +2,9 @@
 
 # Replace RPG starter project with this code when new instructions are live
 
+
+
+""" Used in the GoRandom() function """
 import random
 
 def showInstructions():
@@ -25,7 +28,12 @@ def showStatus():
         print('You see a ' + rooms[currentRoom]['item'])
     print("---------------------------")
 
-#if you get this right, you acquire an escape potion!
+"""
+This code will execute if you enter the garden and pick up the paper
+
+if you get this riddle right, you acquire an escape potion!
+The escape potion is meant to allow you to escape the monster ONE time
+"""
 def riddle():
     #checks the input of the user with correct answer
     if input('what word is spelled incorrectly in the dictionary?') == 'incorrectly':
@@ -73,72 +81,90 @@ currentRoom = 'Hall'
 
 showInstructions()
 
+
+
+"""
+The purpose of this function is to take you to another room, it may be the exact same one that you're in now 
+or any other key in the rooms dictionary
+
+-JC
+"""
 def goRandom():
     print('You\'re teleporting to a random room')
 
     #this takes you to any key in the dictionary
     #that key will be a room we go to
-    currentRoom = random.choice(list(rooms))
+    
+    room_list  = list(rooms)
+    print(room_list)
+    randomRoom = random.choice(room_list)
+    print(randomRoom)
+    currentRoom = randomRoom
+
 
 #loop forever
 while True:
 
     showStatus()
 
-  #get the player's next 'move'
-  #.split() breaks it up into an list array
-  #eg typing 'go east' would give the list:
-  #['go','east']
+    #get the player's next 'move'
+    #.split() breaks it up into an list array
+    #eg typing 'go east' would give the list:
+    #['go','east']
     move = ''
     while move == '':
-      move = input('>')
+        move = input('>')
 
-  # split allows an items to have a space on them
-  # get golden key is returned ["get", "golden key"]          
+    # split allows an items to have a space on them
+    # get golden key is returned ["get", "golden key"]          
     move = move.lower().split(" ", 1)
 
-  #if they type 'go' first
+    #if they type 'go' first
     if move[0] == 'go':
-      #check that they are allowed wherever they want to go
+        #check that they are allowed wherever they want to go
         if move[1] in rooms[currentRoom]:
-        #set the current room to the new room
+            #set the current room to the new room
             currentRoom = rooms[currentRoom][move[1]]
         
-        #this changes the room
+       
+    # This else if statement allows the user to go to any room dependent on the random function 
+        
         elif move[1] == 'random':
             goRandom()
     #there is no door (link) to the new room
         else:
             print('You can\'t go that way!')
 
-  #if they type 'get' first
+    #if they type 'get' first
     if move[0] == 'get' :
-      #if the room contains an item, and the item is the one they want to get
+        #if the room contains an item, and the item is the one they want to get
         if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-        #add the item to their inventory
+            #add the item to their inventory
             inventory += [move[1]]
-      #display a helpful message
+            #display a helpful message
             print(move[1] + ' got!')
-      #delete the item from the room
+            #delete the item from the room
             del rooms[currentRoom]['item']
-      #if the got the riddle 
+            #if the got the riddle 
             if move[1] == 'paper': 
                 riddle()
-    #otherwise, if the item isn't there to get
+        #otherwise, if the item isn't there to get
         else:
-    #tell them they can't get it
+            #tell them they can't get it
             print('Can\'t get ' + move[1] + '!')
 
-  ## Define how a player can win
+    ## Define how a player can win
     if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
         print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
         break
 
-## If a player enters a room with a monster
+    ## If a player enters a room with a monster
     #if player has an escape, it will be saved from the monster
     elif 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'escape' in inventory:
+        #removes the escape from the inventory so that next time the player encounters the 
+        #monster, it may be game over
         inventory.remove('escape')
-        goRandom()
+        print(f'you have used to \'escape\' to hide from the monster. Next time you may not be so lucky!') 
     elif 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
         print('A monster has got you... GAME OVER!')
         break
